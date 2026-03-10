584b631c5c7ea37a2ba0febaf0cafcd2f71ab29d
ChaseJyOyOI <xphuphirayy@gmail.com>
2026-03-10 19:56:30 +0700

    Add new script loading from external URL

--- BFkaitunV1.lua
+++ BFkaitunV1.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xphuphirayy-hue/Kyxhub/refs/heads/main/Notify"))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/xphuphirayy-hue/tast/refs/heads/main/FAS'))()
