86a873883eecdaa22a36bea237f7e82a7c5a3e0c
ChaseJyOyOI <xphuphirayy@gmail.com>
2026-03-13 12:27:10 +0700

    Update BFkaitunV1.lua

--- BFkaitunV1.lua
+++ BFkaitunV1.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xphuphirayy-hue/Kyxhub/refs/heads/main/Notify"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xphuphirayy-hue/Kyxhub/refs/heads/main/BF/main.lua"))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/xphuphirayy-hue/tast/refs/heads/main/FAS'))()
